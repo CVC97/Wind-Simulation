167a60d85648d2ae3a15f64456fadc2bf0dc928c
Carlo <cvcarnap@gmail.com>
2024-04-13 11:07:17 +0200

    WindSimulation: Pressure center in place.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simulating Winds
+
+This web-based simulation is supposed to follow an air masses trajectory under influence of the pressure gradiens force $\Vec{F}_\mathrm{Grad}$, coriolis force $\Vec{F}_\mathrm{Cor}$, and friction force \Vec{F}_\mathrm{Fr}.
+
+Parameters will be chosen according to the users preferences.
--- windsim.js
+++ windsim.js
@@ -1,6 +1,15 @@
 // building canvas
-var canvas = document.getElementById('WindCanvas');
+const canvas = document.getElementById('WindCanvas');
 const ctx = canvas.getContext("2d");
+
+
+// geometric classes
+class PressureCenter {
+    constructor (x, y, color) {
+
+    }
+}
+
 
 
 // functions to transform calculation coordinates (center (0,0), x-range [-8, 8], y-width [-5, 5])
@@ -14,5 +23,21 @@ function y2c(y) {
 
 
 // parameters of the system
+x_high = 3
+y_high = 0
+x_low = -3
+y_high = 0
 
 
+
+ctx.beginPath();
+ctx.arc(x2c(x_high), y2c(y_high), 50, 0, 2 * Math.PI);
+ctx.strokeStyle = "#FC6255";
+ctx.lineWidth = 4;
+ctx.stroke();
+
+ctx.font = "48px serif";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.strokeStyle = "#FC6255";
+ctx.strokeText("H", x2c(x_high+0.01), y2c(y_high-0.03));
